431ed57bc03a49a624b9cd544ebb90d0eae768e9
ckorley4 <ckorley4uo@yahoo.com>
2024-01-14 08:21:24 -0500

    Components Basics new

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
     </nav>
-  );
+  )
 }
 
 function Home() {
@@ -13,20 +13,28 @@ function Home() {
     <div id="home">
       <h1>Home</h1>
     </div>
-  );
+  )
 }
 
-{/* write an <About> component here */}
-
+const About = () => (
+  <div id="about">
+    <h2>About</h2>
+  </div>
+)
+{
+  /* write an <About> component here */
+}
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
